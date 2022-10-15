c379b9b85431449d8e88f12cd4b00aa2794e69a7
timohaverich <timo.haverich@fh-bielefeld.de>
2022-10-15 15:55:27 +0200

    Mit Kommentaren

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,60 @@
-/*
- * Berechnet den Wochentag aus einem angegebenen Datum (Tag, Monat, Jahr) mit einer entsprechenden Formel.
- * Die Monate Januar und Februar werden separat betrachtet und angepasst, sodass die Formel weiterhin greift.
- * Die Formel gibt den Index des Wochentages zurück, welcher dem zugehörigen String des Wochentages zugeordnet
- * und ausgegeben wird.
- */
-
 import java.util.Scanner;
 
 public class Main {
 
+    // Methode zum Filtern der eingegebenen Messwerte
     static void filterMesswerte() {
 
         Scanner sc = new Scanner(System.in); // neues Scanner-Objekt
 
         System.out.println("Geben Sie einen Messwert ein.");
 
-        int einsCounter = 0;
-        int eingabeWert = 0;
-        int letzteEingabe = 0;
-        int letztenZweiWerte = 0;
+        int einsCounter = 0; // Variable zum Checken der Endbedingung
+        int eingabeWert = 0; // Variable zum Speichern der aktuellen Eingabe
+        int letzteEingabe = 0; // Variable zum Speichern der letzten Eingabe
+        int letztenZweiWerte = 0; // Variable zum Speichern der letzten zwei Werte
 
-        int ausgabe = 0;
+        int ausgabe = 0; // Variable zum Speichern der Ausgabe
 
-        while(einsCounter < 3) {
-            letzteEingabe = eingabeWert;
-            letztenZweiWerte = ausgabe % 10;
+        // Wird ausgeführt bis die Folge 1,1,1 eingegeben wird
+        while (einsCounter < 3) {
+            letzteEingabe = eingabeWert; // Speichert letzte Eingabe
+            letztenZweiWerte = ausgabe % 10; // Speichert letzten zwei Stellen der Ausgabe
 
-            eingabeWert = sc.nextInt();
+            eingabeWert = sc.nextInt(); // Abfrage der Eingabe
 
+            // Steuerung der Endbedingung bis Eingabe 1,1,1
             if (eingabeWert == 1) {
-                einsCounter = einsCounter + 1;
-            } else {
-                einsCounter = 0;
+                einsCounter = einsCounter + 1; // Erhöht den einsCounter um eins
+            }
+            else {
+                einsCounter = 0; // einsCounter wird wieder auf 0 gesetzt, wenn Eingabe keine 1
             }
 
-            if (eingabeWert < -9 || eingabeWert > 9) {
+            // Filtert die eingegebenen Werte und fügt diese zur Ausgabe hinzu
+            if (eingabeWert < -9 || eingabeWert > 9) { // Prüft ob Eingabe eine einstellige Zahl ist
                 System.out.println("Bitte ganze einstellige Zahl zwischen -9 und 9 eingeben!");
             }
             else if (eingabeWert < 0) {
-                eingabeWert = eingabeWert * -1;
-                ausgabe = ausgabe * 10 + eingabeWert;
+                eingabeWert = eingabeWert * -1; // Berechnet den Betrag negativer Zahlen
+                ausgabe = ausgabe * 10 + eingabeWert; // Fügt den Eingabewert zur Ausgabe hinzu
             }
-            else if (eingabeWert == 0 && (letzteEingabe != 0 && letztenZweiWerte != 0)) {
-                ausgabe = ausgabe * 10 + eingabeWert;
+            else if (eingabeWert == 0 && (letzteEingabe != 0 && letztenZweiWerte != 0)) { // Fasst aufeinander folgende Nullen zusammen
+                ausgabe = ausgabe * 10 + eingabeWert; // Fügt den Eingabewert zur Ausgabe hinzu
             }
 
-            System.out.println("Output: " + ausgabe);
+            System.out.println("Output: " + ausgabe); // Gibt die Ausgabe nach jeder Eingabe aus
         }
 
+        // Ausgabe der gefilterten Werte nach Erfüllung der Endbedingung
         System.out.println("--------------------");
         System.out.println("Endbedingung erreicht. Die gefilterten Messwerte sind: ");
         System.out.println("Output: " + ausgabe);
-
 
     }
 
 
     public static void main(String[] args) {
-        filterMesswerte();
+        filterMesswerte(); // Ruft Methode filterMesswerte auf
     }
 }
