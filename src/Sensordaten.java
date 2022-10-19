4b6836bb2e4d5551e1415b33eb8baf034b4a940f
timohaverich <timo.haverich@fh-bielefeld.de>
2022-10-19 14:19:55 +0200

    Fehlerbehebung

--- src/Sensordaten.java
+++ src/Sensordaten.java
@@ -9,17 +9,12 @@ public class Sensordaten {
 
         System.out.println("Geben Sie einen Messwert ein.");
 
-        int einsCounter = 0; // Variable zum Checken der Endbedingung
+        int einsCounter = 0; // Zähler für die Endbedingung 1,1,1
         int eingabeWert = 0; // Variable zum Speichern der aktuellen Eingabe
-        int ausgabe = 0; // Variable zum Speichern der Ausgabe
-        int letzteEingabe = 0; // Variable zum Speichern der letzten Eingabe
-        int letztenZweiWerte = 0; // Variable zum Speichern der letzten zwei Werte
-
+        int nullCounter = 0; // Zähler für die eingegebenen Nullen
 
         // wird ausgeführt bis die Folge 1,1,1 eingegeben wird
         while (einsCounter < 3) {
-            letzteEingabe = eingabeWert; // speichert letzte Eingabe
-            letztenZweiWerte = ausgabe % 10; // speichert letzten zwei Stellen der Ausgabe
 
             eingabeWert = sc.nextInt(); // Abfrage der Eingabe
 
@@ -31,37 +26,21 @@ public class Sensordaten {
                 einsCounter = 0; // einsCounter wird wieder auf 0 gesetzt, wenn Eingabe keine 1
             }
 
-            // Prüft, ob die Eingabe nicht valide ist und wirft einen Fehler
-            if (eingabeWert < -9 || eingabeWert > 9) { // prüft ob Eingabe eine einstellige Zahl ist
-                System.err.println("Bitte ganze einstellige Zahl zwischen -9 und 9 eingeben!");
-                eingabeWert = sc.nextInt();
-            }
-
             // filtert die eingegebenen Werte und fügt diese zur Ausgabe hinzu
             if (eingabeWert < 0) {
-                eingabeWert *= -1; // berechnet den Betrag negativer Zahlen
-                ausgabe = ausgabe * 10 + eingabeWert; // fügt den Eingabewert zur Ausgabe hinzu
-                System.out.println("Output: " + ausgabe); // gibt die Ausgabe nach jeder Eingabe aus
+                nullCounter = 0; // setzt den nullCounter auf 0
+                einsCounter = 0; // setzt den einsCounter auf 0
+                eingabeWert *= -1; // berechnet den Betrag der eingegebenen Zahl
+                System.out.println("Output: " + eingabeWert); // gibt den gefilterten Wert aus
             }
 
-            if (eingabeWert == 0 && (letzteEingabe != 0 && letztenZweiWerte != 0)) { // fasst aufeinander folgende Nullen zusammen
-                ausgabe = ausgabe * 10 + eingabeWert; // fügt den Eingabewert zur Ausgabe hinzu
-                System.out.println("Output: " + ausgabe); // gibt die Ausgabe nach jeder Eingabe aus
+            // prüft, ob eine Null eingegeben wurde
+            if (eingabeWert == 0 && nullCounter == 0) {
+                System.out.println("Output: 0");
+                nullCounter++; // erhöht den nullCounter um 1
             }
-
-            if (ausgabe == 0 && eingabeWert == 0) {
-                System.out.println("Output: " + ausgabe); // gibt die Ausgabe nach jeder Eingabe aus
-            }
-
         }
-
-        // Ausgabe der gefilterten Werte nach Erfüllung der Endbedingung
-        System.out.println("--------------------");
-        System.out.println("Endbedingung erreicht. Die gefilterten Messwerte sind: ");
-        System.out.println("Output: " + ausgabe);
-
     }
-
 
     public static void main(String[] args) {
         filterMesswerte(); // ruft Methode filterMesswerte auf
